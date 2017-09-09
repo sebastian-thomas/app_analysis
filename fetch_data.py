4c8a3d24684596a53d2fed1b365de68890c70465
Sebastian Thomas <sebastianthomas1@live.com>
2017-09-09 14:28:38 +0530

    basic connections

--- fetch_data.py
+++ fetch_data.py
@@ -3,28 +3,33 @@ from bs4 import BeautifulSoup
 from database import init_db, db_session
 from models import AppDetails
 from sqlalchemy.exc import IntegrityError
+import rake
+import operator
+import json
 
-def getDetailsForApp(app_id):
-    print ('Fetching details for :' + app_id)
+def getDetailsForApp(socketio,app_id):
+    socketio.emit ('log', {"msg" : "Fetching details for :" + app_id})
     app_from_db = AppDetails.query.filter_by(package=app_id).first()
     if app_from_db is not None:
         print ("Cached")
-        return
+        return app_from_db.desc
     app_page_url = "https://play.google.com/store/apps/details?id=" + app_id
     app_page = requests.get(app_page_url)
     soup = BeautifulSoup(app_page.content, 'html.parser')
     details = soup.find('div', class_='show-more-content text-body')
-    print (details.text.encode("utf-8"))
+    #print (details.text.encode("utf-8"))
     app_d = AppDetails(app_id, details.text)
     try:
         db_session.add (app_d)
         db_session.commit()
     except IntegrityError:
         db_session.rollback()
-        print ("Already exists "+ app_id)
+        #print ("Already exists "+ app_id)
+    return details.text
 
 
-def getDataFromPlayStore(search_param):
+def getDataFromPlayStore(socketio, search_param):
+    text = ""
     apps = []
     search_results_url = "https://play.google.com/store/search?c=apps&q=" + search_param
     search_results_page = requests.get(search_results_url)
@@ -40,10 +45,20 @@ def getDataFromPlayStore(search_param):
     #fetch details for each app
 
     for app_id in apps:
-        getDetailsForApp(app_id)
+        text = text + getDetailsForApp(socketio,app_id)
 
+    return text
+
+def getKeywords (socketio,search_term):
+    text = getDataFromPlayStore(socketio,search_term)
+    socketio.emit ('log', {"msg": "Extracting keywords"})
+    rake_object = rake.Rake("SmartStoplist.txt", 3,4,4)
+    keywords = rake_object.run(text)
+    socketio.emit ('keywords', json.dumps(keywords))
+    return keywords
 
 if __name__ == "__main__":
     init_db()
-    getDataFromPlayStore("food")
-    #getDetailsForApp("com.application.zomato")
+    keywords = getKeywords ("food")
+    for keyword in keywords:
+        print (keyword)
--- server.py
+++ server.py
@@ -1,14 +1,26 @@
-
+import sys
 from flask import Flask
 from flask import request
 from flask import render_template
-from database import db_session
+from database import init_db, db_session
+from flask_socketio import SocketIO
+from flask_socketio import send, emit
+
+from fetch_data import getKeywords
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secretseb'
+socketio = SocketIO(app)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@socketio.on('search_event') 
+def search(data):
+    print(data["term"], file=sys.stderr)
+    emit('log', {"msg":"Got search term " + data["term"]})
+    getKeywords (socketio, data["term"])
 
 @app.route('/search',  methods=['GET', 'POST'])
 def search():
@@ -17,3 +29,8 @@ def search():
 @app.teardown_appcontext
 def shutdown_session(exception=None):
     db_session.remove()
+
+
+if __name__ == '__main__':
+    init_db()
+    socketio.run(app)
